16b76f04b34059dff59bc806587fb59ae4f07227
Terje Io <terjeio@users.noreply.github.com>
2024-02-06 15:07:11 +0100

    Updated for API change.

--- Src/btt_skr_mini_e3_2.0.c
+++ Src/btt_skr_mini_e3_2.0.c
@@ -4,20 +4,20 @@
 
   Part of grblHAL
 
-  Copyright (c) 2021-2023 Terje Io
+  Copyright (c) 2021-2024 Terje Io
 
-  Grbl is free software: you can redistribute it and/or modify
+  grblHAL is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  Grbl is distributed in the hope that it will be useful,
+  grblHAL is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+  along with grblHAL. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
@@ -90,9 +90,8 @@ void board_init (void)
 
     io_stream_t const *stream;
 
-    if((stream = stream_open_instance(TRINAMIC_STREAM, 115200, NULL)) == NULL) {
+    if((stream = stream_open_instance(TRINAMIC_STREAM, 115200, NULL, "Trinamic UART")) == NULL)
         stream = stream_null_init(115200);
-    }
 
     memcpy(&tmc_uart, stream, sizeof(io_stream_t));
     tmc_uart.disable_rx(true);
