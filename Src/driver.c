b703aec646b946746f0d6e29c48bf11b31bbfb87
Terje Io <terjeio@users.noreply.github.com>
2025-04-05 17:30:17 +0200

    Updated to use new low level ioports HAL/API.

--- Src/driver.c
+++ Src/driver.c
@@ -281,7 +281,6 @@ static bool IOInitDone = false;
 static spindle_id_t spindle_id = -1;
 #endif
 #if DRIVER_SPINDLE_ENABLE & SPINDLE_PWM
-static bool pwmEnabled = false;
 static spindle_pwm_t spindle_pwm;
 #endif
 static pin_group_pins_t limit_inputs = {0};
@@ -935,7 +934,7 @@ static void probeConfigure (bool is_probe_away, bool probing)
 
 #ifdef AUX_DEVICES
 
-    if(hal.signals_cap.probe_triggered) {
+    if(hal.driver_cap.probe_latch) {
         probe.is_probing = Off;
         probe.triggered = hal.probe.get_state().triggered;
         pin_irq_mode_t irq_mode = probing && !probe.triggered ? (probe.inverted ? IRQ_Mode_Falling : IRQ_Mode_Rising) : IRQ_Mode_None;
@@ -1034,12 +1033,15 @@ static bool aux_claim_explicit (aux_ctrl_t *aux_ctrl)
         ioport_assign_function(aux_ctrl, &((input_signal_t *)aux_ctrl->input)->id);
 #ifdef PROBE_PIN
         if(aux_ctrl->function == Input_Probe) {
+
+            xbar_t *pin = hal.port.get_pin_info(Port_Digital, Port_Input, aux_ctrl->aux_port);
+
             probe_port = aux_ctrl->aux_port;
             hal.probe.get_state = probeGetState;
             hal.probe.configure = probeConfigure;
             hal.probe.connected_toggle = probeConnectedToggle;
             hal.driver_cap.probe_pull_up = On;
-            hal.signals_cap.probe_triggered = aux_ctrl->irq_mode != IRQ_Mode_None;
+            hal.signals_cap.probe_triggered = hal.driver_cap.probe_latch = (pin->cap.irq_mode & aux_ctrl->irq_mode) == aux_ctrl->irq_mode;
         }
 #endif
 #if defined(SAFETY_DOOR_PIN)
@@ -1072,14 +1074,33 @@ bool aux_out_claim_explicit (aux_ctrl_out_t *aux_ctrl)
 
 // Static spindle (off, on cw & on ccw)
 
-inline static void spindle_off (void)
+inline static void spindle_off (spindle_ptrs_t *spindle)
 {
+    spindle->context.pwm->flags.enable_out = Off;
+#ifdef SPINDLE_DIRECTION_PIN
+    if(spindle->context.pwm->flags.cloned) {
+        BITBAND_PERI(SPINDLE_DIRECTION_PORT->ODR, SPINDLE_DIRECTION_PIN) = settings.pwm_spindle.invert.ccw;
+    } else {
+        BITBAND_PERI(SPINDLE_ENABLE_PORT->ODR, SPINDLE_ENABLE_PIN) = settings.pwm_spindle.invert.on;
+    }
+#elif defined(SPINDLE_ENABLE_PIN)
     BITBAND_PERI(SPINDLE_ENABLE_PORT->ODR, SPINDLE_ENABLE_PIN) = settings.pwm_spindle.invert.on;
+#endif
 }
 
-inline static void spindle_on (void)
+inline static void spindle_on (spindle_ptrs_t *spindle)
 {
+    spindle->context.pwm->flags.enable_out = On;
+#ifdef SPINDLE_DIRECTION_PIN
+    if(spindle->context.pwm->flags.cloned) {
+        BITBAND_PERI(SPINDLE_DIRECTION_PORT->ODR, SPINDLE_DIRECTION_PIN) = !settings.pwm_spindle.invert.ccw;
+    } else {
+        BITBAND_PERI(SPINDLE_ENABLE_PORT->ODR, SPINDLE_ENABLE_PIN) = !settings.pwm_spindle.invert.on;
+    }
+#elif defined(SPINDLE_ENABLE_PIN)
     BITBAND_PERI(SPINDLE_ENABLE_PORT->ODR, SPINDLE_ENABLE_PIN) = !settings.pwm_spindle.invert.on;
+#endif
+
 }
 
 inline static void spindle_dir (bool ccw)
@@ -1094,13 +1115,13 @@ inline static void spindle_dir (bool ccw)
 // Start or stop spindle
 static void spindleSetState (spindle_ptrs_t *spindle, spindle_state_t state, float rpm)
 {
-    UNUSED(spindle);
+    UNUSED(rpm);
 
     if(!state.on)
-        spindle_off();
+        spindle_off(spindle);
     else {
         spindle_dir(state.ccw);
-        spindle_on();
+        spindle_on(spindle);
     }
 }
 
@@ -1108,37 +1129,39 @@ static void spindleSetState (spindle_ptrs_t *spindle, spindle_state_t state, flo
 
 // Variable spindle control functions
 
+static void pwm_off (spindle_ptrs_t *spindle)
+{
+    if(spindle->context.pwm->flags.always_on) {
+        SPINDLE_PWM_TIMER_CCR = spindle->context.pwm->off_value;
+#if SPINDLE_PWM_TIMER_N == 1 || SPINDLE_PWM_TIMER_N == 8
+        SPINDLE_PWM_TIMER->BDTR |= TIM_BDTR_MOE;
+#endif
+        SPINDLE_PWM_TIMER_CCR = spindle->context.pwm->off_value;
+    } else
+#if SPINDLE_PWM_TIMER_N == 1 || SPINDLE_PWM_TIMER_N == 8
+        SPINDLE_PWM_TIMER->BDTR &= ~TIM_BDTR_MOE; // Set PWM output low
+#else
+        SPINDLE_PWM_TIMER_CCR = 0;
+#endif
+}
+
 // Sets spindle speed
 static void spindleSetSpeed (spindle_ptrs_t *spindle, uint_fast16_t pwm_value)
 {
-    if (pwm_value == spindle->context.pwm->off_value) {
-        pwmEnabled = false;
-        if(spindle->context.pwm->settings->flags.enable_rpm_controlled) {
-            if(spindle->context.pwm->flags.cloned)
-                spindle_dir(false);
-            else
-                spindle_off();
-        }
-        if(spindle->context.pwm->flags.always_on) {
-            SPINDLE_PWM_TIMER_CCR = spindle->context.pwm->off_value;
-#if SPINDLE_PWM_TIMER_N == 1 || SPINDLE_PWM_TIMER_N == 8
-            SPINDLE_PWM_TIMER->BDTR |= TIM_BDTR_MOE;
-#endif
-            SPINDLE_PWM_TIMER_CCR = pwm_value;
+    if(pwm_value == spindle->context.pwm->off_value) {
+
+        if(spindle->context.pwm->flags.rpm_controlled) {
+            spindle_off(spindle);
+            if(spindle->context.pwm->flags.laser_off_overdrive)
+                SPINDLE_PWM_TIMER_CCR = spindle->context.pwm->pwm_overdrive;
         } else
-#if SPINDLE_PWM_TIMER_N == 1 || SPINDLE_PWM_TIMER_N == 8
-            SPINDLE_PWM_TIMER->BDTR &= ~TIM_BDTR_MOE; // Set PWM output low
-#else
-            SPINDLE_PWM_TIMER_CCR = 0;
-#endif
+            pwm_off(spindle);
+
     } else {
-        if(!pwmEnabled) {
-            if(spindle->context.pwm->flags.cloned)
-                spindle_dir(true);
-            else
-                spindle_on();
-            pwmEnabled = true;
-        }
+
+        if(!spindle->context.pwm->flags.enable_out && spindle->context.pwm->flags.rpm_controlled)
+            spindle_on(spindle);
+
         SPINDLE_PWM_TIMER_CCR = pwm_value;
 #if SPINDLE_PWM_TIMER_N == 1 || SPINDLE_PWM_TIMER_N == 8
         SPINDLE_PWM_TIMER->BDTR |= TIM_BDTR_MOE;
@@ -1154,20 +1177,21 @@ static uint_fast16_t spindleGetPWM (spindle_ptrs_t *spindle, float rpm)
 // Start or stop spindle
 static void spindleSetStateVariable (spindle_ptrs_t *spindle, spindle_state_t state, float rpm)
 {
+    if(!(spindle->context.pwm->flags.cloned ? state.ccw : state.on)) {
+        spindle_off(spindle);
+        pwm_off(spindle);
+    } else {
 #ifdef SPINDLE_DIRECTION_PIN
-    if(state.on || spindle->context.pwm->flags.cloned)
-        spindle_dir(state.ccw);
+        if(!spindle->context.pwm->flags.cloned)
+            spindle_dir(state.ccw);
 #endif
-    if(!spindle->context.pwm->settings->flags.enable_rpm_controlled) {
-        if(state.on)
-            spindle_on();
-        else
-            spindle_off();
+        if(rpm == 0.0f && spindle->context.pwm->flags.rpm_controlled)
+            spindle_off(spindle);
+        else {
+            spindle_on(spindle);
+            spindleSetSpeed(spindle, spindle->context.pwm->compute_value(spindle->context.pwm, rpm, false));
+        }
     }
-
-    spindleSetSpeed(spindle, state.on || (state.ccw && spindle->context.pwm->flags.cloned)
-                              ? spindle->context.pwm->compute_value(spindle->context.pwm, rpm, false)
-                              : spindle->context.pwm->off_value);
 }
 
 bool spindleConfig (spindle_ptrs_t *spindle)
@@ -1216,7 +1240,7 @@ bool spindleConfig (spindle_ptrs_t *spindle)
         SPINDLE_PWM_TIMER->CR1 |= TIM_CR1_CEN;
 
     } else {
-        if(pwmEnabled)
+        if(spindle->context.pwm->flags.enable_out)
             spindle->set_state(spindle, (spindle_state_t){0}, 0.0f);
         spindle->set_state = spindleSetState;
     }
@@ -1240,6 +1264,9 @@ static spindle_state_t spindleGetState (spindle_ptrs_t *spindle)
     state.ccw = (SPINDLE_DIRECTION_PORT->IDR & SPINDLE_DIRECTION_BIT) != 0;
 #endif
     state.value ^= settings.pwm_spindle.invert.mask;
+#ifdef SPINDLE_PWM_PIN
+    state.on |= spindle->param->state.on;
+#endif
 
     return state;
 }
@@ -1843,7 +1870,7 @@ bool driver_init (void)
 #else
     hal.info = "STM32F103CB";
 #endif
-    hal.driver_version = "250328";
+    hal.driver_version = "250404";
     hal.driver_url = GRBL_URL "/STM32F1xx";
 #ifdef BOARD_NAME
     hal.board = BOARD_NAME;
